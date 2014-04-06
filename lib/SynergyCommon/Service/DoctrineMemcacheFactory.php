df0a24ef4384f1f5839ad8cc5c46c68575e061a0
odiaseo <peleodiase@yahoo.co.uk>
2014-04-06 23:46:36 +0100

    Create site aware doctrine cache

--- lib/SynergyCommon/Service/DoctrineMemcacheFactory.php
+++ lib/SynergyCommon/Service/DoctrineMemcacheFactory.php
@@ -1,6 +1,8 @@
 <?php
 namespace SynergyCommon\Service;
 
+use Doctrine\Common\Cache\MemcacheCache;
+use Zend\Http\Request;
 use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
 
@@ -9,14 +11,29 @@ class DoctrineMemcacheFactory
 {
     public function createService(ServiceLocatorInterface $serviceLocator)
     {
-        /** @var $site \SynergyCommon\Entity\AbstractEntity */
-        $site     = $serviceLocator->get('active_site');
-        $cache    = new MemcacheService();
+        $host = '';
+        /** @var $request \Zend\Http\PhpEnvironment\Request */
+        $request = $serviceLocator->get('application')->getRequest();
+
+        if ($request instanceof Request) {
+            /** @var $event \Zend\Mvc\MvcEvent */
+            $event = $serviceLocator->get('application')->getMvcEvent();
+
+            if ($rm = $event->getRouteMatch()) {
+                $host = $rm->getParam('host');
+            }
+        } else {
+            $host = $request->getServer('HTTP_HOST');
+        }
+
+        $prefix = preg_replace('/[^a-z0-9]/i', '', $host);
+
+        $cache    = new MemcacheCache();
         $memcache = new \Memcache();
 
         $memcache->connect('localhost', 11211);
         $cache->setMemcache($memcache);
-        $cache->setSite($site);
+        $cache->setNamespace($prefix);
 
         return $cache;
     }
