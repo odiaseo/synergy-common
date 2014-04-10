df14647732c12ebca04eeb92fc8fe9300e71ec26
odiaseo <peleodiase@yahoo.co.uk>
2014-04-10 22:53:21 +0100

    set locale length

---
+++ lib/SynergyCommon/Service/DoctrineApcCacheFactory.php
@@ -0,0 +1,35 @@
+<?php
+namespace SynergyCommon\Service;
+
+use Doctrine\Common\Cache\ApcCache;
+use Zend\Console\Request;
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+class DoctrineApcFactory
+    implements FactoryInterface
+{
+    public function createService(ServiceLocatorInterface $serviceLocator)
+    {
+        $host = '';
+        /** @var $request \Zend\Http\PhpEnvironment\Request */
+        $request = $serviceLocator->get('application')->getRequest();
+
+        if ($request instanceof Request) {
+            /** @var $event \Zend\Mvc\MvcEvent */
+            $event = $serviceLocator->get('application')->getMvcEvent();
+            if ($event && $rm = $event->getRouteMatch()) {
+                $host = $rm->getParam('host');
+            }
+        } else {
+            $host = $request->getServer('HTTP_HOST');
+        }
+
+        $prefix = preg_replace('/[^a-z0-9]/i', '', $host);
+
+        $cache = new ApcCache();
+        $cache->setNamespace($prefix);
+
+        return $cache;
+    }
+}
