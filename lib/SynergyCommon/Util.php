137ecadb7a30a73f347e746720ebe0721e697962
odiaseo <peleodiase@yahoo.co.uk>
2014-04-17 19:26:30 +0100

    Added utility class

---
+++ lib/SynergyCommon/Util.php
@@ -0,0 +1,24 @@
+<?php
+namespace SynergyCommon;
+
+class Util
+{
+
+    /**
+     * Ensure string is returned
+     *
+     * @param $value
+     *
+     * @return array|string
+     */
+    public static function ensureIsString($value)
+    {
+        if (is_array($value)) {
+            return $value;
+        } elseif (is_array($value)) {
+            return implode(',', array_filter($value));
+        } else {
+            return (string)$value;
+        }
+    }
+}
