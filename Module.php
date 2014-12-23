fcdc38004e53074ea8dab61fd39531e22b884b53
odiaseo <peleodiase@yahoo.co.uk>
2014-12-23 20:27:07 +0000

    changed dir name

--- Module.php
+++ Module.php
@@ -16,4 +16,20 @@ namespace SynergyCommon;
  */
 class Module {
 
+	/**
+	 * @return array
+	 */
+	public function getConfig() {
+		return include __DIR__ . '/config/module.config.php';
+	}
+
+	public function getAutoloaderConfig() {
+		return array(
+			'Zend\Loader\StandardAutoloader' => array(
+				'namespaces' => array(
+					__NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+				),
+			),
+		);
+	}
 }
