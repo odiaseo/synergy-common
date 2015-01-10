26ac10b28c5e13a51ca73db958b1ac0b84d19f6c
odiaseo <peleodiase@yahoo.co.uk>
2015-01-10 18:03:52 +0000

    updated entity

--- src/SynergyCommon/Member/Entity/UserGroup.php
+++ src/SynergyCommon/Member/Entity/UserGroup.php
@@ -12,52 +12,44 @@ use SynergyCommon\Entity\AbstractEntity;
  * @ORM\Entity
  *
  */
-class UserGroup
-    extends AbstractEntity
-{
-    /**
-     * @ORM\Id
-     * @ORM\Column(type="integer");
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
-    protected $id;
-    /**
-     * @ORM\Column(type="string")
-     */
-    protected $title;
-    /**
-     * @ORM\Column(type="string")
-     */
-    protected $description;
+class UserGroup extends AbstractEntity {
+	/**
+	 * @ORM\Id
+	 * @ORM\Column(type="integer");
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 */
+	protected $id;
+	/**
+	 * @ORM\Column(type="string")
+	 */
+	protected $title;
+	/**
+	 * @ORM\Column(type="string")
+	 */
+	protected $description;
 
-    public function setDescription($description)
-    {
-        $this->description = $description;
-    }
+	public function setDescription( $description ) {
+		$this->description = $description;
+	}
 
-    public function getDescription()
-    {
-        return $this->description;
-    }
+	public function getDescription() {
+		return $this->description;
+	}
 
-    public function setId($id)
-    {
-        $this->id = $id;
-    }
+	public function setId( $id ) {
+		$this->id = $id;
+	}
 
-    public function getId()
-    {
-        return $this->id;
-    }
+	public function getId() {
+		return $this->id;
+	}
 
-    public function setTitle($title)
-    {
-        $this->title = $title;
-    }
+	public function setTitle( $title ) {
+		$this->title = $title;
+	}
 
-    public function getTitle()
-    {
-        return $this->title;
-    }
+	public function getTitle() {
+		return $this->title;
+	}
 
 }
