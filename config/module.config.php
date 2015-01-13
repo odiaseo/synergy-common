c33a198280cc5a4cbbb113b2c1e4f8b7b859004f
Pele Odiase <pele.odiase@valtech.co.uk>
2015-01-13 03:28:28 +0000

    moved entities away

--- config/module.config.php
+++ config/module.config.php
@@ -8,52 +8,52 @@
  */
 
 return array(
-	'service_manager' => array(
-		'abstract_factories' => array(
-			'SynergyCommon\Model\AbstractModelFactory',
-			'SynergyCommon\Service\AbstractServiceFactory',
-			'SynergyCommon\Entity\AbstractEntityFactory',
-		),
-		'invokables'         => array(
-			'synergycommon\entity\licence' => 'SynergyCommon\Entity\BaseLicence',
-			'synergycommon\entity\site'    => 'SynergyCommon\Entity\BaseSite',
-		),
-		'factories'          => array(
-			'logger'                              => 'SynergyCommon\Service\LoggerFactory',
-			'doctrine.cache.synergy_memcache'     => 'SynergyCommon\Service\DoctrineMemcacheFactory',
-			'doctrine.cache.synergy_apc'          => 'SynergyCommon\Service\DoctrineApcCacheFactory',
-			'doctrine.cache.cache\factory'        => 'SynergyCommon\Service\DoctrineCacheFactory',
-			'doctrine.cache.result\cache\factory' => 'SynergyCommon\Service\DoctrineResultCacheFactory',
-		)
-	),
-	'session'         => array(
-		'config'       => array(
-			'class'   => 'Zend\Session\Config\SessionConfig',
-			'options' => array(
-				'name' => 'synergycommon',
-			),
-		),
-		'save_handler' => array(
-			'cache' => array(
-				'adapter' => array(
-					'name'    => 'filesystem',
-					'options' => array(
-						'cache_dir' => __DIR__ . '/../data/session',
-					)
-				)
-			),
-		),
-		'lifetime'     => 7200,
-		'storage'      => 'Zend\Session\Storage\SessionArrayStorage',
-		'validators'   => array(
-			'Zend\Session\Validator\RemoteAddr',
-			'Zend\Session\Validator\HttpUserAgent',
-		),
-	),
-	'synergy'         => array(
-		'memcache' => array(
-			'host' => 'localhost',
-			'port' => 11211
-		)
-	)
+    'service_manager' => array(
+        'abstract_factories' => array(
+            'SynergyCommon\Model\AbstractModelFactory',
+            'SynergyCommon\Service\AbstractServiceFactory',
+            'SynergyCommon\Entity\AbstractEntityFactory',
+        ),
+        'invokables'         => array(
+            'synergycommon\entity\licence' => 'SynergyCommon\Entity\BaseLicence',
+            'synergycommon\entity\site'    => 'SynergyCommon\Entity\BaseSite',
+        ),
+        'factories'          => array(
+            'logger'                              => 'SynergyCommon\Service\LoggerFactory',
+            'doctrine.cache.synergy_memcache'     => 'SynergyCommon\Service\DoctrineMemcacheFactory',
+            'doctrine.cache.synergy_apc'          => 'SynergyCommon\Service\DoctrineApcCacheFactory',
+            'doctrine.cache.cache\factory'        => 'SynergyCommon\Service\DoctrineCacheFactory',
+            'doctrine.cache.result\cache\factory' => 'SynergyCommon\Service\DoctrineResultCacheFactory',
+        )
+    ),
+    'session'         => array(
+        'config'       => array(
+            'class'   => 'Zend\Session\Config\SessionConfig',
+            'options' => array(
+                'name' => 'synergycommon',
+            ),
+        ),
+        'save_handler' => array(
+            'cache' => array(
+                'adapter' => array(
+                    'name'    => 'filesystem',
+                    'options' => array(
+                        'cache_dir' => __DIR__ . '/../data/session',
+                    )
+                )
+            ),
+        ),
+        'lifetime'     => 7200,
+        'storage'      => 'Zend\Session\Storage\SessionArrayStorage',
+        'validators'   => array(
+            'Zend\Session\Validator\RemoteAddr',
+            'Zend\Session\Validator\HttpUserAgent',
+        ),
+    ),
+    'synergy'         => array(
+        'memcache' => array(
+            'host' => 'localhost',
+            'port' => 11211
+        )
+    )
 );
