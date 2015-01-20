a1ed604de4f296a46be67ec6d4278a689370c17a
Pele Odiase <pele.odiase@valtech.co.uk>
2015-01-20 07:02:20 +0000

    refactor save handler

--- src/SynergyCommon/Model/SessionModel.php
+++ src/SynergyCommon/Model/SessionModel.php
@@ -1,12 +1,16 @@
 <?php
 namespace SynergyCommon\Model;
 
-use SynergyCommon\Model\AbstractModel;
-
-class SessionModel
-    extends AbstractModel
+/**
+ * Class SessionModel
+ *
+ * @package SynergyCommon\Model
+ */
+class SessionModel extends AbstractModel
 {
-
+    /**
+     * @return mixed
+     */
     public function collectGabage()
     {
         $queryBuilder = $this->getEntityManager()->createQueryBuilder();
@@ -18,5 +22,55 @@ class SessionModel
             ->where($where);
 
         return $query->getQuery()->execute();
+    }
+
+    /**
+     * @param $sessionId
+     * @param $name
+     *
+     * @return mixed
+     */
+    public function getSessionRecord($sessionId, $name)
+    {
+        $queryBuilder = $this->getEntityManager()->createQueryBuilder();
+        $query        = $queryBuilder->select('e')
+            ->from($this->getEntity(), 'e')
+            ->where('e.id = :id')
+            ->andWhere('e.name = :name')
+            ->setParameters(
+                array(
+                    'id'   => $sessionId,
+                    'name' => $name
+                )
+            )->setMaxResults(1)
+            ->getQuery();
+        $query->useResultCache(false);
+
+        return $query->getOneOrNullResult();
+    }
+
+    /**
+     * @param $sessionId
+     * @param $sessionName
+     *
+     * @return bool|mixed
+     */
+    public function deleteSession($sessionId, $sessionName)
+    {
+        try {
+            $queryBuilder = $this->getEntityManager()->createQueryBuilder();
+            $query        = $queryBuilder->delete()
+                ->from($this->getEntity(), 'e')
+                ->where('e.id = :id')
+                ->andWhere('e.name = :name')
+                ->setParameter(':name', $sessionName)
+                ->setParameter(':id', $sessionId);
+
+            return $query->getQuery()->execute();
+        } catch (\Exception $exception) {
+            $this->getLogger()->logException($exception);
+
+            return false;
+        }
     }
 }
