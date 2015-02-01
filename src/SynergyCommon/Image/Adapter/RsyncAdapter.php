d2edb1a88b534fa6264f27c929e5c7eba5aa43c7
odiaseo <peleodiase@yahoo.co.uk>
2015-02-01 12:21:33 +0000

    added rsync adapter

---
+++ src/SynergyCommon/Image/Adapter/RsyncAdapter.php
@@ -0,0 +1,48 @@
+<?php
+namespace SynergyCommon\Image\Adapter;
+
+use SynergyCommon\Image\TransferAdapterInterface;
+
+/**
+ * Class RsyncAdapter
+ *
+ * @package SynergyCommon\Image\Adapter
+ */
+class RsyncAdapter implements TransferAdapterInterface
+{
+    /** @var \SynergyCommon\Image\Config\ImageCompressorOptions */
+    public $_options;
+
+    /**
+     * @param \SynergyCommon\Image\Config\ImageCompressorOptions $options
+     */
+    public function setOptions($options)
+    {
+        $this->_options = $options;
+    }
+
+    /**
+     * @return \SynergyCommon\Image\Config\ImageCompressorOptions
+     */
+    public function getOptions()
+    {
+        return $this->_options;
+    }
+
+    public function copy($filename, $destination)
+    {
+        $password = $this->_options->getRemotePassword();
+        $password = $password ? ':' . $password : '';
+
+        $command = sprintf(
+            'rsync -avi --ignore-existing --remove-source-files %s %s%s@%s:%s',
+            $filename,
+            $this->_options->getRemoteUser(),
+            $password,
+            $this->_options->getRemoteHost(),
+            $destination
+        );
+
+        return \shell_exec($command) ? false : true;
+    }
+}
