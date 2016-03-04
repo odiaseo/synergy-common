b7f4edf02f11443d0ea5891697d0f81058e1c5eb
Pele Odiase <pele.odiase@valtech.co.uk>
2016-03-04 18:13:25 +0000

    disable global caching

--- src/SynergyCommon/Doctrine/CacheAwareQueryTrait.php
+++ src/SynergyCommon/Doctrine/CacheAwareQueryTrait.php
@@ -38,7 +38,7 @@ trait CacheAwareQueryTrait
     {
         if ($this->enableResultCache) {
             $query->useResultCache(true, $this->cacheLifetime);
-            $query->setCacheable(true);
+            //$query->setCacheable(true);
             if ($this->cacheKey) {
                 $query->setResultCacheId($this->cacheKey);
             }
